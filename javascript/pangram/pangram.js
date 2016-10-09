f54430339344be147d2df6560a33f820c9a18dbd
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-09 17:08:24 -0600

    [Update] Replace forEach loop with Set

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,14 +3,11 @@ var Pangram = function(input) {
 };
 
 Pangram.prototype.isPangram = function() {
-  var letters = Object.create(null);
   const cleanString = this.originalSentence
     .toLowerCase()
     .replace(/[^a-z]/g, '');
-  cleanString.split('').forEach(function(letter) {
-    letters[letter] = letters[letter] + 1 || 1;
-  });
-  return Object.keys(letters).length === 26;
+
+  return new Set(cleanString).size === 26;
 };
 
 module.exports = Pangram;
