09ff25ded2d268c9108176b41130ee142db74525
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-17 10:51:15 -0700

    Complete robot name exercise

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,45 @@
+const takenNames = [];
+
+const viableLetters = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+const twoRandomLetters = () => {
+  return (
+    `${viableLetters[Math.floor(Math.random() * viableLetters.length)]}` +
+    `${viableLetters[Math.floor(Math.random() * viableLetters.length)]}`
+  );
+};
+
+const threeRandomNumbers = () => {
+  return (
+    `${Math.floor(Math.random() * 10)}` +
+    `${Math.floor(Math.random() * 10)}` +
+    `${Math.floor(Math.random() * 10)}`
+  );
+};
+
+class Robot {
+  constructor(){
+    this.name = this.generateRandomName();
+  }
+
+  generateRandomName(){
+    const name = `${twoRandomLetters()}${threeRandomNumbers()}`;
+    if (takenNames.includes(name)) {
+      this.generateRandomName();
+    } else {
+      takenNames.push(name);
+      return name;
+    }
+  }
+
+  reset(){
+    this.name = this.generateRandomName();
+  }
+
+}
+
+module.exports = Robot;
