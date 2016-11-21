2766adb2de34ff097552279f9a58d95040df83c5
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-21 13:57:44 -0700

    Additional refactor for readability

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,9 +1,24 @@
 class Grains
-  def self.square(square_number)
-    2**(square_number - 1)
+  FIRST_SQUARE = 1
+  LAST_SQUARE  = 64
+
+  def self.square(n)
+    if n < FIRST_SQUARE || n > LAST_SQUARE
+      raise ArgumentError, "#{n} is invalid. Must be between " \
+                           "#{FIRST_SQUARE} and #{LAST_SQUARE}"
+    end
+    number_of_grains(n)
   end
 
   def self.total
-    1.upto(64).reduce(0) { |total, n| total += square(n) }
+    FIRST_SQUARE.upto(LAST_SQUARE).reduce(0) do |total_grains, n|
+      total_grains += square(n)
+    end
   end
+
+  def self.number_of_grains(n)
+    2**(n - 1)
+  end
+
+  private_class_method :number_of_grains
 end
