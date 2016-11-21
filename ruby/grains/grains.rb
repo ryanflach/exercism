be05e3bf38c4ff5e0e2d4e92f289d672597b0373
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-21 08:55:31 -0700

    Refactor grains solution

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,20 +1,9 @@
 class Grains
   def self.square(square_number)
-    return square_number if square_number < 3
-    grains = 2
-    3.upto(square_number) do |n|
-      grains *= 2
-    end
-    grains
+    2**(square_number - 1)
   end
 
   def self.total
-    total_grains = 0
-    current = 1
-    while current <= 64
-      total_grains += square(current)
-      current += 1
-    end
-    total_grains
+    1.upto(64).reduce(0) { |total, n| total += square(n) }
   end
 end
