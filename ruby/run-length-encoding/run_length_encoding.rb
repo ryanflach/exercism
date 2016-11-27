609a5386f96f18f51cad9d17edd9a71988e132a4
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-26 17:02:18 -0800

    Complete run length encoding challenge - Refactor to simplify logic and utilize scan method with a regex

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,33 +1,14 @@
 class RunLengthEncoding
   def self.encode(input)
-    output, previous, count = '', '', 0
-    input.chars.each_with_index do |char, index|
-      at_last_char = index == input.length - 1
-      if char == previous
-        count += 1
-        at_last_char ? output += "#{count}#{previous}" : next
-      else
-        output += count > 1 ? "#{count}#{previous}" : previous
-        return output += char if at_last_char
-      end
-      previous = char
-      count = 1
+    input.scan(/((.)\2*)/).reduce('') do |output, (chain, char)|
+      output += chain.length > 1 ? "#{chain.length}#{char}" : char
     end
-    output
   end
 
   def self.decode(input)
-    output, current = '', ''
-    input.chars.each do |char|
-      if ('0'..'9').to_a.include?(char)
-        current += char
-      else
-        num_chars = current.to_i
-        num_chars < 2 ? output += char : num_chars.times { output += char }
-        current = ''
-      end
+    input.scan(/(\d*)(.)/).reduce('') do |output, (length, char)|
+      output += length.to_i > 1 ? char * length.to_i : char
     end
-    output
   end
 end
 
