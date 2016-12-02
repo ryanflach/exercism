06df20e735a4fd862a2aceca6ae2b22832472dbc
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-02 12:14:28 -0700

    Complete clock exercise - Add ability to add and subtract minutes from a clock

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -3,20 +3,12 @@ const hourInDay = 24;
 
 class Clock {
   constructor(hour, min) {
-    if (min < 0) {
-      hour -= Math.ceil(-min / minInHour);
-      min += minInHour;
-    }
-
-    if (min >= minInHour) {
-      hour += Math.floor(min / minInHour);
-    }
-
     this.hour = hour;
     this.min = min;
   }
 
   toString() {
+    this.processOverUnder();
     const formattedHours = formatTime(this.hour, hourInDay);
     const formattedMinutes = formatTime(this.min, minInHour);
     return `${formattedHours}:${formattedMinutes}`;
@@ -25,11 +17,32 @@ class Clock {
   equals(clock) {
     return this.toString() === clock.toString();
   }
+
+  plus(min) {
+    this.min += min;
+    return this;
+  }
+
+  minus(min) {
+    this.min -= min;
+    return this;
+  }
+
+  processOverUnder() {
+    if (this.min < 0) {
+      this.hour -= Math.ceil(-this.min / minInHour);
+      this.min += minInHour;
+    }
+
+    if (this.min >= minInHour) {
+      this.hour += Math.floor(this.min / minInHour);
+    }
+  }
 }
 
 const at = (hour, min = 0) => {
   return new Clock(hour, min);
-}
+};
 
 const formatTime = (input, qualifier) => {
   if (input < 0) {
