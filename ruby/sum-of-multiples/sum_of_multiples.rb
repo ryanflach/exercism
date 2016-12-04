71c97fc4cad530412cd1388d84d10aa9aadbec73
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-04 10:58:06 -0700

    Remove unnecessary conditional within the select block

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -5,7 +5,7 @@ class SumOfMultiples
 
   def to(ending_number)
     (1...ending_number).select do |number|
-      number if divisible_by.any? { |n| number % n == 0 }
+      divisible_by.any? { |n| number % n == 0 }
     end.reduce(0, :+)
   end
 
