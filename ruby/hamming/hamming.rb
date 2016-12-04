19b87ea8a730badc55abf783a81523e2411353df
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-04 16:20:54 -0700

    Refactor to class - A call to the class method compute now creates and instance and calls compute_difference on that instance - Output details of error in error message - Refactor reduce into select method

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,33 @@
-module Hamming
+class Hamming
+  def initialize(strand_one, strand_two)
+    @strand_one = strand_one
+    @strand_two = strand_two
+    validate_input
+  end
+
   def self.compute(strand_one, strand_two)
-    raise ArgumentError if strand_one.length != strand_two.length
-    location = 0
-    strand_one.chars.reduce(0) do |total, char|
-      total += 1 if char != strand_two[location]
-      location += 1
-      total
+    new(strand_one, strand_two).compute_difference
+  end
+
+  def compute_difference
+    strand_one.chars.select.with_index do |char, index|
+      char != strand_two[index]
+    end.length
+  end
+
+  private
+
+  attr_reader :strand_one,
+              :strand_two
+
+  def validate_input
+    if strand_one.length != strand_two.length
+      raise ArgumentError,
+        "Length of strands must be identical. " \
+        "#{strand_one.length} != #{strand_two.length}"
     end
   end
+
 end
 
 module BookKeeping
