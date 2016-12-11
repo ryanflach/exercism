5c4f82d9bff8b46f1962198ec989e789af8d989f
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-12 07:20:56 +0900

    Complete scrabble score exercise

---
+++ javascript/scrabble-score/scrabble-score.js
@@ -0,0 +1,40 @@
+const LETTER_VALUES = {
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+};
+
+const score = (input) => {
+  if (input) {
+    return input.split('').reduce((total, char) => {
+      return total += LETTER_VALUES[char.toUpperCase()];
+    }, 0);
+  } else {
+    return 0;
+  }
+};
+
+module.exports = score;
