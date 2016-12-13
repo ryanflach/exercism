c703f76188973af2b36819f36d7aeffa8cd4e35e
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-13 09:34:31 +0900

    Update to ES6 syntax

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,15 +1,17 @@
-var Isogram = function(word) {
-  this.input = word;
-};
+class Isogram {
+  constructor(word) {
+    this.input = word;
+  }
 
-Isogram.prototype.isIsogram = function() {
-  var cleanChars = this.input.toLowerCase()
-    .replace(/[\s-]/g, '')
-    .split('');
-  var unique = cleanChars.filter(function(letter, index, array) {
-    return array.indexOf(letter) === index;
-  });
-  return unique.length === cleanChars.length;
-};
+  isIsogram() {
+    const cleanChars = this.input.toLowerCase()
+                               .replace(/[\s-]/g, '')
+                               .split('');
+    const unique = cleanChars.filter((letter, index, array) => {
+      return array.indexOf(letter) === index;
+    });
+    return unique.length === cleanChars.length;
+  }
+}
 
 module.exports = Isogram;
