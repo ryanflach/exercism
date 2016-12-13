e39f20812083bde120a6c5583ce85197dec93094
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-13 09:32:36 +0900

    Update to ES6 syntax

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,15 +1,15 @@
-var Bob = function() {};
-
-Bob.prototype.hey = function(input) {
-  if (input === input.toUpperCase() && input.search(/[A-z]/) !== -1) {
-    return 'Whoa, chill out!';
-  } else if (input.charAt(input.length - 1) === '?') {
-    return 'Sure.';
-  } else if (input.replace(/\s*/, '').length === 0){
-    return 'Fine. Be that way!';
-  } else {
-    return 'Whatever.';
+class Bob {
+  hey(input) {
+    if (input === input.toUpperCase() && input.search(/[A-z]/) !== -1) {
+      return 'Whoa, chill out!';
+    } else if (input.charAt(input.length - 1) === '?') {
+      return 'Sure.';
+    } else if (input.replace(/\s*/, '').length === 0){
+      return 'Fine. Be that way!';
+    } else {
+      return 'Whatever.';
+    }
   }
-};
+}
 
 module.exports = Bob;
